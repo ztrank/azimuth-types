05ae60f6b19f94660c32f862c5a0dcab45d3c8d3
Zachary Trank-Zelewicz <trank.zachary@gmail.com>
2019-08-29 14:55:05 -0400

    Initial Commit

---
+++ spec/types.spec.ts
@@ -0,0 +1,2 @@
+import 'reflect-metadata';
+test('Nothing to test!', () => {});
---
+++ src/TYPES.ts
@@ -0,0 +1,19 @@
+export const Types = Object.freeze({
+    AppSettings: 'AppSettings',
+    WebSettings: 'WebSettings',
+    MachineSettings: 'MachineSettings',
+    KmsClient: 'KmsClient',
+    KmsStorage: 'KmsStorage',
+    SecretService: 'SecretService',
+    ConnectionFactory: 'ConnectionFactory',
+    DataLayerFactory: 'DataLayerFactory',
+    HttpContext: 'HttpContext',
+    HttpRegistrar: 'HttpRegistrar',
+    AuthAdminService: 'AuthAdminService',
+    AuthService: 'AuthService',
+    CharacterAdminService: 'CharacterAdminService',
+    CharacterService: 'CharacterService',
+    EnvironmentAdminService: 'EnvironmentAdminService',
+    EnvironmentService: 'EnvironmentService',
+    TravellerService: 'TravellerService'
+});
